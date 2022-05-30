cddd6313611daad9568b01fce9b6cf45fc6516d7
Jade <hendricksjade@gmail.com>
2022-05-30 16:19:16 +0200

    adding in the create a category action and posting it to the DB

--- BulkyBook/BulkBookWeb/Controllers/CategoryController.cs
+++ BulkyBook/BulkBookWeb/Controllers/CategoryController.cs
@@ -16,5 +16,22 @@ namespace BulkBookWeb.Controllers
             IEnumerable<Category> objCategoryList = _db.Categories; 
             return View(objCategoryList);
         }
+
+        //Get Action Method
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+
+        //Post Action Method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
