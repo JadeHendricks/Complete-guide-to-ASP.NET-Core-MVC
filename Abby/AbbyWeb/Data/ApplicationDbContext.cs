7f4f59a170c8b2909fd514d377ab9573c0b7ea71
Jade <hendricksjade@gmail.com>
2022-06-05 17:12:50 +0200

    adding in the category model, connection string and migrating the catgeory table to the DB

---
+++ Abby/AbbyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AbbyWeb.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace AbbyWeb.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        //passing the options from program.cs to the base class of dbContext
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Category { get; set; }
+    }
+}
---
+++ Abby/AbbyWeb/Model/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AbbyWeb.Model
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int Dsiplayorder { get; set; }
+    }
+}
