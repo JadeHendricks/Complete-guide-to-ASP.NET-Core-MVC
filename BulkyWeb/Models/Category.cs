98edd2fb059d18d67f3c829a0e7c1c9f7988b876
Jade <hendricksjade@gmail.com>
2023-04-13 14:42:47 +0200

    add(categoryModel): Adding in a new category model and taking a look into data annotations.

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        //adding key here allows the property below to be the primary key
+        //with entity framework ID will automatically be the primary key
+        //also if the name is the name of the class plus ID it will also be made as the primary key automatically = CategoryId
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }    
+        public int DisplayOrder { get; set; }
+    }
+}
