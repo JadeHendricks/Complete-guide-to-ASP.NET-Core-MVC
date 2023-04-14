38c63ef01ac84f28eef33926598fe348365d0061
Jade <hendricksjade@gmail.com>
2023-04-14 15:28:04 +0200

    add(category): Adding in edit and delete buttons, adding in a new edit page and adding in functionality to allow editing of categories.

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -50,7 +50,45 @@ namespace BulkyWeb.Controllers
             }
             //stay on the page you're on
             return View();
+        }
 
+        //Edit Category action
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            //retrieve one category from the DB
+            //find only works on the primary key
+            //if you want to find other things use FirstOrDefault();
+            Category? categoryFromDb = _db.Categories.Find(id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            //check that all the parts follow the requirements set in the model
+            if (ModelState.IsValid)
+            {
+                //updating the category to the table queue
+                _db.Categories.Update(obj);
+                //saving the category to the table (whatever is in the queue)
+                //it will automatically update all the fields present inside of obj inside of the table
+                _db.SaveChanges();
+                //redirect to index inside of category class
+                return RedirectToAction("Index", "Category");
+            }
+            //stay on the page you're on
+            return View();
         }
     }
 }
