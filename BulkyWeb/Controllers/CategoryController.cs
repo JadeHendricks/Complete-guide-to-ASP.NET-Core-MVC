0085ec47edd7a9d4d752507bd99a4d4d2402c2af
Jade <hendricksjade@gmail.com>
2023-04-14 14:43:04 +0200

    add(categories): Adding in create category functionality, looking into server side and client side validation for forms.

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -25,5 +25,32 @@ namespace BulkyWeb.Controllers
         {
             return View();
         }
+
+        //whenever something is being posted, this endpoint will be invoked
+        //when then get the category object from the form as @model Category gives the form this object model
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            //custom validation
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                //adding custom validation to the current modelState
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the name");
+            }
+
+            //check that all the parts follow the requirements set in the model
+            if(ModelState.IsValid)
+            {
+                //adding the category to the table queue
+                _db.Categories.Add(obj);
+                //saving the category to the table (whatever is in the queue)
+                _db.SaveChanges();
+                //redirect to index inside of category class
+                return RedirectToAction("Index", "Category");
+            }
+            //stay on the page you're on
+            return View();
+
+        }
     }
 }
