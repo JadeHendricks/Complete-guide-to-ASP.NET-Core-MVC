55901c8fb3674d1ef73ee84e1414466093ed3e45
Jade <hendricksjade@gmail.com>
2023-04-16 01:01:49 +0200

    add(IRepository): Adding and implementingthe IRepository interface.

---
+++ Bulky.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        //T will be category or any other generic model on which we want to perform certain operations
+        IEnumerable<T> GetAll();
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entity);
+    }
+}
---
+++ Bulky.DataAccess/Repository/Repository.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq.Expressions;
+using Bulky.DataAccess.Repository.IRepository;
+using BulkyWeb.DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bulky.DataAccess.Repository
+{
+    internal class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbSet;
+
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            //this is like writing this - _db.Categories == dbSet, whatever we pass tot he generic will correlate to the table
+            //so now when we want to add, remove, edit etc we will call that on the dbSet (dbSet.Add()) as dbSet === <T> what is passed + _db.<T>
+            this.dbSet = _db.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            //ie - _db.categories.Where(u=>u.Id === id).FirstOrDefault(); - _db.Set<T>() aka _dbSet.Categories.Where().FirstOrDefault
+            IQueryable<T> query = dbSet;
+            query = query.Where(filter);
+            return query.FirstOrDefault();
+
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            //List<Category> objCategoryList = _db.Categories.ToList(); - aka _dbSet.Categories.ToList()
+            IQueryable<T> query = dbSet;
+            return query.ToList();
+        }
+
+        public void Remove(T entity)
+        {
+            //_db.Set<T>() - aka _dbSet.Categories.Remove(entity)
+            dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entity)
+        {
+            //the RemoveRange expects a IEnumerable and it will remove all everything that exists in the IEnumerable
+            //_db.Set<T>() - aka _dbSet.Categories.RemoveRange(entity)
+            dbSet.RemoveRange(entity);
+        }
+    }
+}
