96e2b56a7814df651d6c08a3c436b5a69aa7318c
Jade <hendricksjade@gmail.com>
2023-04-16 01:27:09 +0200

    add(IRepository): adding and implementing the ICategoryRepository interface.

---
+++ Bulky.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,27 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using BulkyWeb.DataAccess.Data;
+using System.Linq.Expressions;
+
+namespace Bulky.DataAccess.Repository
+{
+    //doing this allows us to only implement the methods we want extra and not everything from Repository + CategoryRepository - only CategoryRepository
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category obj)
+        {
+            _db.Categories.Update(obj);
+        }
+    }
+}
